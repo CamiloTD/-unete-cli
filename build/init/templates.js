f88ffb635c121adcf9d276351b2c5ef568f64dce
CamiloTD <camilotd1999@gmail.com>
2020-05-02 22:32:01 -0500

    Add Javascript Repo

--- build/init/templates.js
+++ build/init/templates.js
@@ -2,6 +2,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const termx_1 = require("termx");
 const clone = require('git-shallow-clone');
 const REPO_TYPESCRIPT = "https://github.com/CamiloTD/-unete-cli-typescript.git";
+const REPO_JAVASCRIPT = "https://github.com/CamiloTD/-unete-cli-javascript.git";
 function Typescript() {
     clone(REPO_TYPESCRIPT, process.cwd(), () => {
         termx_1.log("Done!");
@@ -10,5 +11,9 @@ function Typescript() {
 }
 exports.Typescript = Typescript;
 function Javascript() {
+    clone(REPO_JAVASCRIPT, process.cwd(), () => {
+        termx_1.log("Done!");
+        setTimeout(() => process.exit(0), 1000);
+    });
 }
 exports.Javascript = Javascript;
--- init/templates.ts
+++ init/templates.ts
@@ -1,16 +1,22 @@
 import { log } from "termx";
 
 const clone = require('git-shallow-clone')
+
 const REPO_TYPESCRIPT = "https://github.com/CamiloTD/-unete-cli-typescript.git";
+const REPO_JAVASCRIPT = "https://github.com/CamiloTD/-unete-cli-javascript.git";
 
 export function Typescript () {
     clone(REPO_TYPESCRIPT, process.cwd(), () => {
         log("Done!");
-        
-        setTimeout(() =>process.exit(0), 1000);
+
+        setTimeout(() => process.exit(0), 1000);
     });
 }
 
 export function Javascript () {
-    process.exit(0);
+    clone(REPO_JAVASCRIPT, process.cwd(), () => {
+        log("Done!");
+
+        setTimeout(() => process.exit(0), 1000);
+    });
 }
